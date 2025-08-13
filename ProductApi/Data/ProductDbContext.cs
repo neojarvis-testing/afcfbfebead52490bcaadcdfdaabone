381474afa8d7c4d89d7cc24973f4e23265e8e814
neojarvis <techadmin@iamneo.ai>
2025-08-13 14:49:55 +0000

    Auto-save-commit-13/08/2025--20:19:50

---
+++ ProductApi/Data/ProductDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using ProductApi.Models;
+
+namespace ProductApi.Data;
+
+public class ProductDbContext : DbContext
+{
+    public ProductDbContext(DbContextOptions<ProductDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Product> Products => Set<Product>();
+}
---
+++ ProductApi/Models/Product.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ProductApi.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+
+    [Required]
+    [StringLength(100)]
+    public string Name { get; set; } = string.Empty;
+
+    [StringLength(500)]
+    public string? Description { get; set; }
+
+    [Required]
+    [Range(0.01, double.MaxValue)]
+    public decimal Price { get; set; }
+
+    [Required]
+    public int StockQuantity { get; set; }
+}
